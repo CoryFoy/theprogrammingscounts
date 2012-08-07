104791c56cafd2c01e74a5efdccae27e5a6cd9c6
Cory Foy <github@cornetdesign.com>
2012-08-06 21:27:19 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theprogrammingscouts::Application.config.secret_token = '15d7d12e178ec42ef71131521fed09b8c374e39328a684d1951d8274591386e6be95435b6640848dce7c30db0cdaab725aa5bcecf46f5bee5752f94399d6e527'
